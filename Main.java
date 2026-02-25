cfc93ef6fdcc507a95cb42220f7a7a116c020240
ASHPAK ABDUL INAMDAR <161122245+Ashpak02@users.noreply.github.com>
2026-02-25 11:19:08 +0530

    Update greeting in Main class

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Ashpak");
+        System.out.println("Hello, World! Ashpak Inamdar");
     }
 
 }
+
